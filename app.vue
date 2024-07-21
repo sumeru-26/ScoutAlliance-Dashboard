df179b413ad44a543a58d200a76aa168b825e1eb
sumeru-26 <155037091+sumeru-26@users.noreply.github.com>
2024-07-21 15:21:48 -0700

    added loading bar

--- app.vue
+++ app.vue
@@ -2,7 +2,7 @@
 
   // required for displaying 'toast' dialogues (see https://www.shadcn-vue.com/docs/components/toast.html)
   import Toaster from '@/components/ui/toast/Toaster.vue'
-
+  
   import { useColorMode } from '@vueuse/core'
 
   const mode = useColorMode()
@@ -12,7 +12,8 @@
 </script>
 
 <template>
-  <div>
+  <NuxtLoadingIndicator color="#fafafa" />
+  <NuxtLayout>
     <NuxtPage />
-  </div>
+  </NuxtLayout>
 </template>
