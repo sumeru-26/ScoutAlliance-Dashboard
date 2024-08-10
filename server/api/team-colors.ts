c8b4ed405bcb5346f24aee6d5b21335506bc71a6
sumeru-26 <155037091+sumeru-26@users.noreply.github.com>
2024-08-10 09:29:37 -0700

    updated endpoints to use new token util

--- server/api/team-colors.ts
+++ server/api/team-colors.ts
@@ -1,10 +1,12 @@
 import { defineEventHandler, H3Event, parseCookies } from 'h3'
 
 export default defineEventHandler(async (event: H3Event) => {
-    // TODO: update to use new auth method
-    // if possible, set it up as a util
+
     const cookies = parseCookies(event)
-    const team = cookies?.user
+    const token = cookies['auth:token']
+    const team = await getTeamFromToken(token)
+
     return fetch("https://api.frc-colors.com/v1/team/" + team)
     .then(response => response.json())
+    
 })
