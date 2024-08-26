da72e73c7d2f9062ea1bf4b246348ba13cdb92b1
sumeru-26 <155037091+sumeru-26@users.noreply.github.com>
2024-08-26 14:56:09 -0700

    removed _id from appearing in entries

--- server/api/data.ts
+++ server/api/data.ts
@@ -7,7 +7,7 @@ export default defineEventHandler(async (event: H3Event) => {
     const team = await getTeamFromToken(token)
     
     const col = dbClient.db('entries').collection(`${team}`)
-    const dataCursor = await col.find({})
+    const dataCursor = await col.find({}, { projection: { _id: 0 } })
     return await dataCursor.toArray()
     
 })
