8562cf379996ec965fd9f94d30107a95c15075eb
sumeru-26 <155037091+sumeru-26@users.noreply.github.com>
2024-09-08 21:30:26 -0700

    updated account dropdown in nav bar

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,5 +1,7 @@
 <script setup>
 
+    import { Button } from './ui/button';
+
     import {
         NavigationMenu,
         NavigationMenuContent,
@@ -19,26 +21,12 @@
         DropdownMenuTrigger,
     } from '@/components/ui/dropdown-menu'
 
-    import { Bot } from 'lucide-vue-next'
-
-    const { signOut } = useAuth()
-
-    const loaded = ref(false)
-
-    const { data: rawData } = await useFetch('/api/team-colors')
-    const teamColors = rawData._rawValue
-    const primaryTeamColor = teamColors.primaryHex
-    const secondaryTeamColor = teamColors.secondaryHex
-    const iconBgClass = `rounded-full my-5 mr-5 bg-[${secondaryTeamColor}]`
-    //const iconBgClass = `bg-[#277139] rounded-full`
-    loaded.value = true
+    const { data, signOut } = useAuth()
 
     function logout() {
         // TODO: finish logout
         signOut({ callbackUrl: '/login' })
-        
-    }
-    
+    } 
 
 </script>
 
@@ -63,18 +51,16 @@
                 </NavigationMenuItem>
             </NavigationMenuList>
         </NavigationMenu>
-        <div v-if="loaded" class="w-full flex justify-end">
+        <div class="w-full flex justify-end">
             <DropdownMenu>
                 <DropdownMenuTrigger>
-                    <div :class="iconBgClass">        
-                        <Bot :color="primaryTeamColor" class="m-2" />
-                    </div>    
+                    <Button class="my-5 mr-5">Account</Button>    
                 </DropdownMenuTrigger>
                 <DropdownMenuContent>
-                    <DropdownMenuLabel>{{ team }}</DropdownMenuLabel>
+                    <DropdownMenuLabel>{{ JSON.parse(data).user }}</DropdownMenuLabel>
                     <DropdownMenuSeparator />
                     <DropdownMenuItem>
-                        <span>Account</span>
+                        <span>Settings</span>
                     </DropdownMenuItem>
                     <DropdownMenuItem>
                         <span @click.stop.prevent="logout">Logout</span>
