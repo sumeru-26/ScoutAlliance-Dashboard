86c126d21b11396bc2eb2d356e34a16a075fd7a1
sumeru-26 <155037091+sumeru-26@users.noreply.github.com>
2025-01-02 23:09:51 -0800

    optimized entry loading & added pagination

--- pages/entries.vue
+++ pages/entries.vue
@@ -1,8 +1,26 @@
 <script setup>
 
     import { useToast } from '@/components/ui/toast/use-toast'
+    import {
+        DropdownMenu,
+        DropdownMenuContent,
+        DropdownMenuRadioGroup,
+        DropdownMenuRadioItem,
+        DropdownMenuTrigger
+    } from '@/components/ui/dropdown-menu'
+    import {
+        Pagination,
+        PaginationEllipsis,
+        PaginationFirst,
+        PaginationLast,
+        PaginationList,
+        PaginationListItem,
+        PaginationNext,
+        PaginationPrev,
+    } from '@/components/ui/pagination'
 
-    import { Plus, X, LoaderCircle, Frown } from 'lucide-vue-next';
+
+    import { Plus, X, LoaderCircle, RefreshCcw, Frown, ChevronDown } from 'lucide-vue-next';
 
     useSeoMeta({
         title: 'Entries'
@@ -17,104 +35,161 @@
     const eventQuery = ref('')
     const matchQuery = ref('')
     const botQuery = ref('')
+    const typeQuery = ref('')
     const dataQueries = ref([{field: '', value: '', index: 0}])
 
     let entries = ref()
 
     const loading = ref(true)
 
-    const { status, data: rawEntries, error: err } = await useLazyFetch('/api/data')
+    const currentPageLocal = ref(1)
+    const currentPageServer = ref(1)
+    
+    const entriesPerPage = ref(25)
+    const totalNumberOfEntries = ref(0)
+
+    const query = ref({})
+
+    const { status, data: rawDBResults, refresh: refreshData, error: err } = await useLazyFetch('/api/data', {
+        query: {
+            page: currentPageServer,
+            entriesPerPage: entriesPerPage,
+            dbQuery: query
+        },
+        watch: false
+    })
 
     watch(status, (newStatus, oldStatus) => {
         if (newStatus === 'success') {
-            rawEntries.value.sort((a,b) => b.metadata.timestamp - a.metadata.timestamp)
-            entries.value = rawEntries.value
+            // let re = rawDBResults.value.entries().next().value[1].entries
+            // console.log(rawDBResults.value.entries)
+            // console.log(re)
+            entries.value = rawDBResults.value.entries().next().value[1].entries // praying ts works
+            totalNumberOfEntries.value = rawDBResults.value.entries().next().value[1].metadata[0].numEntries
+            console.log(rawDBResults.value.entries().next().value[1])
+            console.log(totalNumberOfEntries.value)
             loading.value = false
         }
     })
+
+    watch(currentPageLocal, (newPage, _) => {
+        // console.log(newPage)
+        refreshDataWrapper()
+    })
+
+    function refreshDataWrapper() {
+        // console.log('beginning refresh')
+        loading.value = true
+        currentPageServer.value = currentPageLocal.value
+        refreshData().then( function() {
+            loading.value = false
+            // console.log('finished refresh')
+        } )
+        
+    }
+
+    function generateRequestQuery() {
+            const tempQuery = {}
+            for (const [key, value] of Object.entries({ 'metadata.event': eventQuery, 'metadata.match': matchQuery, 'metadata.bot': botQuery, 'metadata.type': typeQuery })) {
+                if (value.value != '') {
+                    if (!isNaN(value.value)) {
+                        tempQuery[key] = +value.value // converts to int, mad sketchy tho
+                    } else {
+                        tempQuery[key] = value.value
+                    }
+                }
+            }
+            for (const q of dataQueries.value) {
+                if (q.field != '') {
+                    tempQuery.value[`data.${q.field}`] = q.value
+                }
+            }
+        // console.log(query.value)
+        query.value = tempQuery
+    }
     
     function submitQuery() {
-
-        entries.value = rawEntries.value.filter((entry) => queryFilter(entry))
+        generateRequestQuery()
+        refreshDataWrapper()
     }
 
-    function queryFilter(entry) {
+    // function queryFilter(entry) {
 
-        if ((eventQuery.value != '' && entry.metadata.event != eventQuery.value)) {
-            return false
-        }
+    //     if ((eventQuery.value != '' && entry.metadata.event != eventQuery.value)) {
+    //         return false
+    //     }
 
-        if (matchQuery.value != '') {
-            const matchQueryVal = parseInt(matchQuery.value)
-            if (isNaN(matchQueryVal)) {
-                toast({
-                    description: 'Match # is not a real number',
-                })
-                return false
-            }
-            else if (entry.metadata.match != matchQueryVal) {
-                return false
-            }
-        }
+    //     if (matchQuery.value != '') {
+    //         const matchQueryVal = parseInt(matchQuery.value)
+    //         if (isNaN(matchQueryVal)) {
+    //             toast({
+    //                 description: 'Match # is not a real number',
+    //             })
+    //             return false
+    //         }
+    //         else if (entry.metadata.match != matchQueryVal) {
+    //             return false
+    //         }
+    //     }
 
-        if (botQuery.value != '') {
-            const botQueryVal = parseInt(botQuery.value)
-            if (isNaN(botQueryVal)) {
-                toast({
-                    description: 'Bot # is not a real number',
-                })
-                return false
-            }
-            else if (entry.metadata.bot != botQueryVal) {
-                return false
-            }
-        }
+    //     if (botQuery.value != '') {
+    //         const botQueryVal = parseInt(botQuery.value)
+    //         if (isNaN(botQueryVal)) {
+    //             toast({
+    //                 description: 'Bot # is not a real number',
+    //             })
+    //             return false
+    //         }
+    //         else if (entry.metadata.bot != botQueryVal) {
+    //             return false
+    //         }
+    //     }
         
-        for (var query of dataQueries.value) {
-            if (query.value == 'true' || query.value == 'false') query.value = (query.value === 'true')
-            if (query.field != '' && query.value != '' && recursiveDataQueryFilter(entry, query.field, query.value) == false) {
-                return false
-            }
-        }
+    //     for (var query of dataQueries.value) {
+    //         if (query.value == 'true' || query.value == 'false') query.value = (query.value === 'true')
+    //         if (query.field != '' && query.value != '' && recursiveDataQueryFilter(entry, query.field, query.value) == false) {
+    //             return false
+    //         }
+    //     }
 
-        return true
+    //     return true
         
-    }
+    // }
 
-    function recursiveDataQueryFilter(entry, field, value) {
-        if (!field.includes('.')) {
-            return Object.hasOwn(entry, field) && entry[field] == value
-        } else {
-            const fieldSplit = field.split('.')
-            if (Object.hasOwn(entry, fieldSplit[0])) {
-                return recursiveDataQueryFilter(entry[fieldSplit[0]], field.substring(field.indexOf('.')+1), value)
-            } else {
-                return false
-            }
-        }
-    }
+    // function recursiveDataQueryFilter(entry, field, value) {
+    //     if (!field.includes('.')) {
+    //         return Object.hasOwn(entry, field) && entry[field] == value
+    //     } else {
+    //         const fieldSplit = field.split('.')
+    //         if (Object.hasOwn(entry, fieldSplit[0])) {
+    //             return recursiveDataQueryFilter(entry[fieldSplit[0]], field.substring(field.indexOf('.')+1), value)
+    //         } else {
+    //             return false
+    //         }
+    //     }
+    // }
 
-    function addDataQuery() {
-        dataQueries.value.push({field: '', value: '', index: dataQueries.value.length})
-        reorderDataQueries()
-    }
+    // function addDataQuery() {
+    //     dataQueries.value.push({field: '', value: '', index: dataQueries.value.length})
+    //     reorderDataQueries()
+    // }
 
-    function deleteDataQuery(index) {
-        if (dataQueries.value.length > 1) {
-            dataQueries.value.splice(index, 1)
-            reorderDataQueries()
-        } else {
-            dataQueries.value = [{field: '', value: '', index: 0}]
-        }
-    }
+    // function deleteDataQuery(index) {
+    //     if (dataQueries.value.length > 1) {
+    //         dataQueries.value.splice(index, 1)
+    //         reorderDataQueries()
+    //     } else {
+    //         dataQueries.value = [{field: '', value: '', index: 0}]
+    //     }
+    // }
 
-    function reorderDataQueries() {
-        var i = 0
-        for (var q of dataQueries.value) {
-            q.index = i
-            i++
-        }
-    }
+    // function reorderDataQueries() {
+    //     var i = 0
+    //     for (var q of dataQueries.value) {
+    //         q.index = i
+    //         i++
+    //     }
+    // }
 
 </script>
 
@@ -126,10 +201,11 @@
         Entries
     </h1>
     <div class="m-5 border border-border rounded-lg">
-        <div class="m-5 grid grid-cols-3 gap-2">
-            <Input type="text" placeholder="Event (use TBA event key, e.g. &quot;2024cmptx&quot;)" v-model="eventQuery" />
+        <div class="m-5 grid grid-cols-4 gap-2">
+            <Input type="text" placeholder="Event (TBA Code)" v-model="eventQuery" />
             <Input type="text" placeholder="Match #" v-model="matchQuery" />
             <Input type="text" placeholder="Bot (Team #)" v-model="botQuery" />
+            <Input type="text" placeholder="Type" v-model="typeQuery" />
             <div class="col-span-2">
                 <ul v-for="query in dataQueries" :key="query">
                     <div class="mb-2 flex gap-2">
@@ -148,7 +224,64 @@
                     </div>
                 </ul>
             </div>
-            <Button @click.stop.prevent="submitQuery()" class="mb-2">Submit</Button>
+            <div class="col-span-2 flex gap-2">
+                <Button @click.stop.prevent="submitQuery()" class="mb-2 grow">Submit</Button>
+                <Button @click.stop.prevent="refreshDataWrapper()" variant="outline" size="icon">
+                    <RefreshCcw class="w-4 h-4" />
+                </Button>
+            </div>
+        </div>
+        <div class="mx-5 flex justify-center">
+            <div class="">
+                <Pagination v-model:page="currentPageLocal" :total="totalNumberOfEntries" :items-per-page="entriesPerPage" :sibling-count="1" show-edges>
+                    <PaginationList v-slot="{ items }" class="flex items-center gap-1">
+                    <PaginationFirst />
+                    <PaginationPrev />
+
+                    <template v-for="(item, index) in items">
+                        <PaginationListItem v-if="item.type === 'page'" :key="index" :value="item.value" as-child>
+                        <Button class="w-10 h-10 p-0" :variant="item.value === currentPageLocal ? 'default' : 'outline'">
+                            {{ item.value }}
+                        </Button>
+                        </PaginationListItem>
+                        <PaginationEllipsis v-else :key="item.type" :index="index" />
+                    </template>
+
+                    <PaginationNext />
+                    <PaginationLast />
+                    </PaginationList>
+                </Pagination>
+            </div>
+            <div class="ml-5 flex gap-2">
+                <DropdownMenu>
+                    <DropdownMenuTrigger as-child>
+                        <Button variant="outline">
+                            {{ entriesPerPage }}&nbsp;<ChevronDown class="w-4 h-4" />
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent>
+                        <DropdownMenuRadioGroup v-model="entriesPerPage">
+                            <DropdownMenuRadioItem value="10">
+                                10
+                            </DropdownMenuRadioItem>
+                            <DropdownMenuRadioItem value="25">
+                                25
+                            </DropdownMenuRadioItem>
+                            <DropdownMenuRadioItem value="50">
+                                50
+                            </DropdownMenuRadioItem>
+                            <DropdownMenuRadioItem value="100">
+                                100
+                            </DropdownMenuRadioItem>
+                        </DropdownMenuRadioGroup>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+                <!-- text input option too if we really want it -->
+                <!-- <Input type="number" v-model="entriesPerPage" class="w-10" /> -->
+                <div class="flex items-center">
+                    <p>{{ (currentPageLocal-1)*entriesPerPage + 1 }} - {{ currentPageLocal*entriesPerPage }} of {{ totalNumberOfEntries }}</p>
+                </div>
+            </div>
         </div>
         <div v-if="loading" class="flex h-32 items-center justify-center mb-5">
             <LoaderCircle v-if="loading" class="animate-spin" />
@@ -161,6 +294,26 @@
                 No results
             </p>
             <Frown class="h-4 w-4 text-muted-foreground flex justify-center mx-1" />
+        </div>
+        <div v-if="!loading && entries.length > 0" class="flex justify-center mb-5">
+            <Pagination v-model:page="currentPageLocal" :total="totalNumberOfEntries" :items-per-page="entriesPerPage" :sibling-count="1" show-edges>
+                <PaginationList v-slot="{ items }" class="flex items-center gap-1">
+                <PaginationFirst />
+                <PaginationPrev />
+
+                <template v-for="(item, index) in items">
+                    <PaginationListItem v-if="item.type === 'page'" :key="index" :value="item.value" as-child>
+                    <Button class="w-10 h-10 p-0" :variant="item.value === currentPageLocal ? 'default' : 'outline'">
+                        {{ item.value }}
+                    </Button>
+                    </PaginationListItem>
+                    <PaginationEllipsis v-else :key="item.type" :index="index" />
+                </template>
+
+                <PaginationNext />
+                <PaginationLast />
+                </PaginationList>
+            </Pagination>
         </div>
     </div>
 </template>
--- server/api/data.ts
+++ server/api/data.ts
@@ -1,13 +1,39 @@
+import { metadata } from '@vueuse/core/metadata.cjs'
 import { defineEventHandler, H3Event, parseCookies } from 'h3'
 
 export default defineEventHandler(async (event: H3Event) => {
+    
+    // console.log('new request')
         
     const cookies = parseCookies(event)
+    const query = getQuery(event)
+    
+    const dbQuery = JSON.parse(String(query.dbQuery))
+    const page = Number(query.page)
+    const entriesPerPage = Number(query.entriesPerPage)
+
     const token = cookies['auth:token']
     const team = await getTeamFromToken(token)
+
+    // console.log(JSON.parse(String(query.dbQuery)))
+    // console.log(JSON.parse(String(query.dbQuery)))
+
+    const pipeline = [
+        { $match: dbQuery },
+        { $sort: { "metadata.timestamp": -1 } },
+        { $facet: {
+            metadata: [ { $count: 'numEntries' } ],
+            entries: [
+                { $skip: (page - 1) * entriesPerPage },
+                { $limit: entriesPerPage },
+                { $unset: [ "_id" ] }
+            ]
+        }}
+        
+    ]
     
     const col = dbClient.db('entries').collection(`${team}`)
-    const dataCursor = await col.find({}, { projection: { _id: 0 } })
-    return await dataCursor.toArray()
+    const data = await col.aggregate(pipeline).toArray()
+    return data
     
 })
